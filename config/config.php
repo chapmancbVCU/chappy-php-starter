78865583c7cc03d097abfca36a38ff56b818c178
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-26 16:26:46 -0400

    #6 - Removed ENV for acces_restricted since it does not change and tested out Env class for some variables

--- config/config.php
+++ config/config.php
@@ -1,4 +1,5 @@
 <?php
+use Core\Lib\Utilities\Env;
 /**
  * Configuration for Chappy.php framework.
  */
@@ -11,18 +12,19 @@
     // If you need to run migrations from the browser, you can set this to true temporarily.
     'run_migrations_from_browser' => $_ENV['RUN_MIGRATIONS_FROM_BROWSER'] ?? false,
 
-    'default_controller' => $_ENV['DEFAULT_CONTROLLER'] ?? 'Home', // Default controller if not set in URL
+    'default_controller' => $_ENV['DEFAULT_CONTROLLER'] ?? 'Home', // Default controller if not set in .env
     'default_layout' => $_ENV['DEFAULT_LAYOUT'] ?? 'main', // Default layout if not set in controller
 
-    'app_domain' => $_ENV['APP_DOMAIN'] ?? '/', // Set this to '/' for a live server
+    'app_domain' => Env::get('APP_DOMAIN') ?? '/', // Set this to '/' for a live server
     'version' => '1.0.9',
-    'site_title' => $_ENV['SITE_TITLE'] ?? 'My App', // Default site title if not set
-    'menu_brand' => $_ENV['MENU_BRAND'] ?? 'My Brand', // Branding for menu
+    'site_title' => Env::get('SITE_TITLE') ?? 'My App', // Default site title if not set
+    'menu_brand' => Env::get('MENU_BRAND') ?? 'My Brand', // Branding for menu
 
-    'access_restricted' => $_ENV['ACCESS_RESTRICTED'] ?? 'Restricted', // Controller for restricted redirects
+    'access_restricted' => 'Restricted', // Controller for restricted redirects
 
     'max_login_attempts' => is_numeric($_ENV['MAX_LOGIN_ATTEMPTS'] ?? null) ? (int) $_ENV['MAX_LOGIN_ATTEMPTS'] : 5,
-    's3_bucket' => $_ENV['S3_BUCKET'] ?? null,
 
     'time_zone' => $_ENV['TIME_ZONE'] ?? 'UTC',
+    
+    's3_bucket' => $_ENV['S3_BUCKET'] ?? null,
  ];
