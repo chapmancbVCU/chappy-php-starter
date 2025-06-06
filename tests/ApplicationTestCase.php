c46223ba26ac9d7d09568f26106c87ab7991163a
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-06 18:23:49 -0400

    #22 - Added ability to seed SQLite database in memory

--- tests/ApplicationTestCase.php
+++ tests/ApplicationTestCase.php
@@ -1,6 +1,10 @@
 <?php
 namespace Tests;
+use Core\DB;
+use Core\Lib\Utilities\Env;
+use Console\Helpers\Migrate;
 use PHPUnit\Framework\TestCase;
+use Database\Seeders\DatabaseSeeder;
 
 /**
  * Abstract class for test cases.
@@ -11,7 +15,31 @@ abstract class ApplicationTestCase extends TestCase {
      *
      * @return void
      */
-    protected function setUp(): void {
+    protected function setUp(): void
+    {
         parent::setUp();
+        
+        DB::connect([
+            'driver'   => Env::get('DB_CONNECTION', 'sqlite'),
+            'database' => Env::get('DB_DATABASE', ':memory:'),
+            'host'     => Env::get('DB_HOST', '127.0.0.1'),
+            'port'     => Env::get('DB_PORT', '3306'),
+            'username' => Env::get('DB_USERNAME', 'root'),
+            'password' => Env::get('DB_PASSWORD', ''),
+            'charset'  => Env::get('DB_CHARSET', 'utf8mb4'),
+        ]);
+        
+        $this->runMigrations();
+        $this->runSeeders();
+    }
+
+    protected function runMigrations(): void
+    {
+        Migrate::migrate();
+    }
+
+    protected function runSeeders(): void
+    {
+        (new DatabaseSeeder())->run();
     }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,15 +1,20 @@
 <?php
 namespace Tests\Unit;
+use Core\DB;
+use App\Models\Users;
+use Tests\ApplicationTestCase;
 use PHPUnit\Framework\TestCase;
 
 /**
  * Undocumented class
  */
-class ExampleTest extends TestCase {
+class ExampleTest extends ApplicationTestCase {
     public function test_unit_example_1() {
         $string1 = 'testing';
         $string2 = 'testing';
 
         $this->assertSame($string1, $string2);
+        $user = Users::findById(1);
+        dd($user);
     }
 }
