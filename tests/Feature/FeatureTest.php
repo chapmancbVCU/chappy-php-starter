a450f47a07ac9a25b54e5dfe55aa1e8b8fc1c19d
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-07 15:51:32 -0400

    #22 - Tidy up function

--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -9,13 +9,9 @@ use Core\Lib\Testing\ApplicationTestCase;
 class FeatureTest extends ApplicationTestCase {
     public function test_feature_example_1() {
         $user = Users::findById(1);
-        // dd($user);
         $name = $user->username;
         $output = $this->controllerOutput('admindashboard', 'details', ['1']);
 
-        $this->assertStringContainsString(
-            'Details for '.$name,
-            $output
-        );
+        $this->assertStringContainsString('Details for '.$name, $output);
     }
 }
