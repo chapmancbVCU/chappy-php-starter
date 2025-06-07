4361a855c597a9025ab8e60f47ab4dd7ef2dd1a7
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-06 20:01:49 -0400

    #22 - Added testing config block

--- config/database.php
+++ config/database.php
@@ -23,6 +23,19 @@ return [
             'strict' => true,
         ],
 
+        'mysql_testing' => [
+            'driver' => 'mysql',
+            'host' => Env::get('DB_HOST') ?? '127.0.0.1',
+            'port' => Env::get('DB_PORT') ?? '3306',
+            'database' => Env::get('DB_DATABASE') ?? 'chappy_php_test',
+            'username' => Env::get('DB_USERNAME') ?? 'test_user',
+            'password' => Env::get('DB_PASSWORD') ?? '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+        ],
+
         'mariadb' => [
             'driver' => 'mysql', // MariaDB is compatible with MySQL driver
             'host' => Env::get('DB_HOST') ?? '127.0.0.1',
