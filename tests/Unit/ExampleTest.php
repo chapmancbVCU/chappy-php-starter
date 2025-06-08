9b1461b5020f6c3f94b4cc04875464745c54189a
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-08 19:26:31 -0400

    #22 - Default example test implementation

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -7,9 +7,6 @@ use PHPUnit\Framework\TestCase;
  */
 class ExampleTest extends TestCase {
     public function test_unit_example_1(): void {
-        $string1 = 'testing';
-        $string2 = 'testing';
-
-        $this->assertSame($string1, $string2);
+        $this->assertTrue(true);
     }
 }
