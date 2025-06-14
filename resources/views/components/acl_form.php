950adb46c50b0a7827e7b030a6a8b38604906bea
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-14 12:54:46 -0400

    #27 - Updated to use route function

--- resources/views/components/acl_form.php
+++ resources/views/components/acl_form.php
@@ -3,7 +3,7 @@
     <?= csrf() ?>
     <?= FormHelper::inputBlock('text', "ACL", 'acl', $this->acl->acl, ['class' => 'form-control input-sm'], ['class' => 'form-group'], $this->displayErrors) ?>
     <div class="col-md-12 text-end pt-3">
-        <a href="<?=env('APP_DOMAIN', '/')?>admindashboard/manageACLs" class="btn btn-default">Cancel</a>
+        <a href="<?=route('admindashboard.manageACLs')?>" class="btn btn-default">Cancel</a>
         <?= FormHelper::submitTag('Update',['class'=>'btn btn-primary']) ?>
     </div>
 </form>
