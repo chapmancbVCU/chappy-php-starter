b4757a11e4d174410d44007da25ba99101e619ee
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-14 12:46:30 -0400

    #27 - Updated to use route function

--- resources/views/auth/login.php
+++ resources/views/auth/login.php
@@ -6,7 +6,7 @@
 <div class="row align-items-center justify-content-center">
     <div class="col-md-6 bg-light p-3">
         <h3 class="text-center">Log In</h3>
-        <form class="form" action="<?=route('auth/login')?>" method="post">
+        <form class="form" action="<?=route('auth.login')?>" method="post">
             <?= csrf() ?>
             <?= errorBag($this->displayErrors) ?>
             <?= FormHelper::inputBlock('text', 'Username', 'username', $this->login->username, ['class' => 'form-control'], ['class' => 'form-group mb-3']); ?>
@@ -14,7 +14,7 @@
             <?= FormHelper::checkboxBlockLabelLeft('Remember Me', 'remember_me', "on", $this->login->getRememberMeChecked(), [], ['class' => 'form-group mb-3']); ?>
             
             <div class="d-flex justify-content-end">
-                <div class="flex-grow-1 text-body">Don't have an account? <a href="<?=route('auth.register')?>Sign Up</a></div>
+                <div class="flex-grow-1 text-body">Don't have an account? <a href="<?=route('auth.register')?>">Sign Up</a></div>
                 <?= FormHelper::submitTag('Login',['class'=>'btn btn-primary']) ?>
             </div>
         </form>
