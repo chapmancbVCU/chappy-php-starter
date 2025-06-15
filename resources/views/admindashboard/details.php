7ce6ac1db91bbb024b7e224de8bf6bbb1923ea1c
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-15 18:35:06 -0400

    #24 - Updated to use asset function

--- resources/views/admindashboard/details.php
+++ resources/views/admindashboard/details.php
@@ -8,7 +8,7 @@ use Core\Lib\Utilities\DateTime; ?>
 <div class="align-items-center justify-content-center mx-auto my-3 w-50">
     <a href="<?=route('admindashboard.index')?>" class="btn btn-sm btn-secondary mb-3">Back</a>
     <?php if($this->profileImage != null):?>
-        <img src="<?=env('APP_DOMAIN', '/').$this->profileImage->url?>"
+        <img src="<?=asset($this->profileImage->url)?>"
             class="img-thumbnail mx-auto my-5 d-block w-50 rounded border border-primary shadow-lg">
         </img>
     <?php endif; ?>
