7c00683387b7bc9529f019813ce0c6737b87e618
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-19 21:50:54 -0400

    #33 - Added test for text emails

--- tests/Feature/EmailTest.php
+++ tests/Feature/EmailTest.php
@@ -12,25 +12,36 @@ class EmailTest extends ApplicationTestCase {
      *
      * @return void
      */
-    public function test_email_is_sent(): void
-    {
-        $mail = new MailerService();
+    // public function test_email_is_sent(): void
+    // {
+    //     $mail = new MailerService();
         
-        // Assert true when using test E-mail service.
-        $this->assertNotTrue($mail->send(
-            'user@example.com', 'Welcome!', '<p>Your account is ready!</p>'
-        ));
-    }
+    //     // Assert true when using test E-mail service.
+    //     $this->assertNotTrue($mail->send(
+    //         'user@example.com', 'Welcome!', '<p>Your account is ready!</p>'
+    //     ));
+    // }
 
-    public function test_email_template(): void {
+    // public function test_email_template(): void {
+    //     $mail = new MailerService();
+    //     $hello = "Hello world";
+    //     $this->assertNotTrue($mail->sendTemplate(
+    //         'user@example.com',
+    //         'Welcome to ChappyPHP',
+    //         'hello',
+    //         ['user' => $hello],
+    //         'test'
+    //     ));
+    // }
+
+    public function test_email_text_template(): void {
         $mail = new MailerService();
         $hello = "Hello world";
         $this->assertNotTrue($mail->sendTemplate(
             'user@example.com',
             'Welcome to ChappyPHP',
-            'hello',
-            ['user' => $hello],
-            'test'
+            'welcome',
+            [],
         ));
     }
 }
