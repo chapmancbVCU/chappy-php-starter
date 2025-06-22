4b0352e2c41fceb1b93b3a163aa8de5bc2a3b771
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-22 15:58:45 -0400

    #33 - Initial commit

---
+++ resources/views/emails/layouts/default.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <style>
+        <?=$style?>
+    </style>
+</head>
+<body>
+    <div class="email-wrapper">
+        <h1 class="center">Template</h1>
+        <?= $content ?>
+    </div>
+</body>
+</html>
