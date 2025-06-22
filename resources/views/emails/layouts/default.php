0b4582f190062ee82b4a1e5b8cf0c6e5e06865f5
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-22 15:59:38 -0400

    #33 - Added e-mail auto generated comment and message to contact admin if not intended recipient

--- resources/views/emails/layouts/default.php
+++ resources/views/emails/layouts/default.php
@@ -10,6 +10,8 @@
     <div class="email-wrapper">
         <h1 class="center">Template</h1>
         <?= $content ?>
+        <hr>
+        <p>This E-mail was auto generated and mailbox may not be monitored.  If you are not the intended recipient contact the site administrator and delete this E-mail.</p>
     </div>
 </body>
 </html>
