c9b5211fffba7fb5e9d060a6efaa5bc76fa64bd2
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-24 20:12:04 -0400

    #33 - Added action for attachments home page

--- resources/views/admindashboard/attachments.php
+++ resources/views/admindashboard/attachments.php
@@ -8,7 +8,7 @@
 
 <!-- Body content between these two function calls. -->
 <?php $this->start('body'); ?>
-<h1>Attachments</h1>
+<h1 class="text-center mb-4">Attachments</h1>
 <table class="table table-striped table-bordered table-hover">
     <thead>
         <th>Original Name</th>
