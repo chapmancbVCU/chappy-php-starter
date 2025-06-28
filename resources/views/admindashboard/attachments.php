346fa6b362a93da17452d07360d0b6788ae9995b
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-28 14:21:02 -0400

    #33 - Centered table

--- resources/views/admindashboard/attachments.php
+++ resources/views/admindashboard/attachments.php
@@ -18,38 +18,40 @@ use App\Models\Users;
         <i class="fa fa-plus"></i> Add Attachment
     </a>
 </h1>
-<table class="table table-striped table-bordered table-hover table-striped table-sm">
-    <thead>
-        <th>Original Name</th>
-        <th>Uploader</th>
-        <th>Size</th>
-        <th></th>
-    </thead>
-    <tbody>
-        <?php foreach($this->attachments as $attachment): ?>
-            <tr>
-                <td>
-                    <a href="<?=route('admindashboard.attachmentDetails', [$attachment->id])?>"><?=$attachment->attachment_name?></a>
-                </td>
-                <td><?=EmailAttachments::uploadUsername($attachment->user_id)?></td>
-                <td><?=EmailAttachments::formatBytes($attachment->size)?></td>
-                <td class="text-end">
-                    <a href="<?=route('admindashboard.editAttachments', [$attachment->id])?>"
-                        class="btn btn-secondary btn-sm"><i class="fas fa-edit"></i> Edit
-                    </a>
-                    <form method="POST"
-                        action="<?=route('admindashboard.deleteAttachment', [$attachment->id])?>"
-                        class="d-inline-block"
-                        onsubmit="return confirm('Are you sure you want to delete this attachment?');">
-                        <?=hidden('id', $attachment->id)?>
-                        <?=csrf()?>
-                        <button type="submit" class="btn btn-danger btn-sm">
-                            <i class="fa fa-trash"></i> Delete
-                        </button>
-                    </form>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+<div class="d-flex justify-content-center">
+    <table class="w-75 table table-striped table-bordered table-hover table-striped table-sm">
+        <thead>
+            <th>Original Name</th>
+            <th>Uploader</th>
+            <th>Size</th>
+            <th></th>
+        </thead>
+        <tbody>
+            <?php foreach($this->attachments as $attachment): ?>
+                <tr>
+                    <td>
+                        <a href="<?=route('admindashboard.attachmentDetails', [$attachment->id])?>"><?=$attachment->attachment_name?></a>
+                    </td>
+                    <td><?=EmailAttachments::uploadUsername($attachment->user_id)?></td>
+                    <td><?=EmailAttachments::formatBytes($attachment->size)?></td>
+                    <td class="text-end">
+                        <a href="<?=route('admindashboard.editAttachments', [$attachment->id])?>"
+                            class="btn btn-secondary btn-sm"><i class="fas fa-edit"></i> Edit
+                        </a>
+                        <form method="POST"
+                            action="<?=route('admindashboard.deleteAttachment', [$attachment->id])?>"
+                            class="d-inline-block"
+                            onsubmit="return confirm('Are you sure you want to delete this attachment?');">
+                            <?=hidden('id', $attachment->id)?>
+                            <?=csrf()?>
+                            <button type="submit" class="btn btn-danger btn-sm">
+                                <i class="fa fa-trash"></i> Delete
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
 <?php $this->end(); ?>
