6808aed425c44b64d9fc199b28237f138c7a39be
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-28 12:48:34 -0400

    #33 - Added attachment details to view

--- resources/views/admindashboard/attachment_details.php
+++ resources/views/admindashboard/attachment_details.php
@@ -1,3 +1,5 @@
+<?php use App\Models\EmailAttachments; ?>
+<?php use Core\Lib\Utilities\DateTime; ?>
 <?php $this->setSiteTitle("Details for " . $this->attachment->attachment_name); ?>
 
 <!-- Head content between these two function calls.  Remove if not needed. -->
@@ -8,5 +10,20 @@
 
 <!-- Body content between these two function calls. -->
 <?php $this->start('body'); ?>
-
+<div class="row align-items-center justify-content-center">
+    <div class="col-md-6 bg-light p-3">
+        <a href="<?=route('admindashboard.attachments')?>" class="btn btn-sm btn-secondary">Back</a>
+        <h2 class="text-center"><?=$this->attachment->attachment_name?></h2>
+        <div class="row">
+            <div class="col-md-6">
+                <p><strong>Created at: </strong><?= DateTime::timeAgo($this->attachment->created_at) ?></p>
+                <p><strong>Updated at: </strong><?= DateTime::timeAgo($this->attachment->updated_at) ?></p>
+                <p><strong>Size: </strong><?= EmailAttachments::formatBytes($this->attachment->size) ?></p>
+                <p><strong>MIME Type: </strong><?= $this->attachment->mime_type ?></p>
+                <p><strong>Uploader: </strong><?= $this->uploader->username ?></p>
+                <p><strong>Description: </strong><?= htmlspecialchars_decode(stripslashes($this->attachment->description)) ?></p>
+            </div>
+        </div>
+    </div>
+</div>
 <?php $this->end(); ?>
