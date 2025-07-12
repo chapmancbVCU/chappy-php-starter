a962322fe68fd76ddfd0a1faa4edd71ae5cd0785
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-12 15:49:46 -0400

    #34 - Removed bool to send mailer

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller {
             $loginModel->assign($this->request->get());
             $loginModel->validator();
             if($loginModel->validationPassed()){
-                $loginModel = AuthService::login($this->request, $loginModel, $_POST['username'], true);
+                $loginModel = AuthService::login($this->request, $loginModel, $_POST['username']);
             }
         }
         $this->view->login = $loginModel;
