929b361822e56a2e0fe71165302af0dae7cbe8cc
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-21 21:39:24 -0400

    Added initial setup for config

--- config/queue.php
+++ config/queue.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
+    'driver' => 'database', // or 'redis'
 
+    'database' => [
+        'dsn' => 'mysql:host=127.0.0.1;dbname=queue_app',
+        'username' => 'root',
+        'password' => '',
+    ],
+
+    'redis' => [
+        'host' => '127.0.0.1',
+        'port' => 6379,
+    ],
 ];
