b7edfb3d38ce6dea2759dbbc65ba2e00284ebecf
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-28 20:52:43 -0400

    Initial commit

---
+++ database/migrations/Migration1753312824.php
@@ -0,0 +1,36 @@
+<?php
+namespace Database\Migrations;
+use Core\Lib\Database\Schema;
+use Core\Lib\Database\Blueprint;
+use Core\Lib\Database\Migration;
+
+/**
+ * Migration class for the queue table.
+ */
+class Migration1753312824 extends Migration {
+    /**
+     * Performs a migration for a new table.
+     *
+     * @return void
+     */
+    public function up(): void {
+        Schema::create('queue', function (Blueprint $table) {
+            $table->id();
+            $table->string('queue')->default('default');
+            $table->text('payload');
+            $table->unsignedInteger('attempts')->default(0);
+            $table->timestamp('reserved_at')->nullable();
+            $table->timestamp('available_at');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Undo a migration task.
+     *
+     * @return void
+     */
+    public function down(): void {
+        Schema::dropIfExists('queue');
+    }
+}
