c6c5d22dd36291db8138e2a04624cea0d96540ef
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-28 21:15:49 -0400

    Removed contents of 'database' array so we use built in connection

--- config/queue.php
+++ config/queue.php
@@ -5,11 +5,7 @@ use Core\Lib\Utilities\Env;
 return [
     'driver' => Env::get('REDIS_DRIVER') ?? 'database',
 
-    'database' => [
-        'dsn' => 'mysql:host=127.0.0.1;dbname=queue_app',
-        'username' => 'root',
-        'password' => '',
-    ],
+    'database' => [],
 
     'redis' => [
         'host' => '127.0.0.1',
