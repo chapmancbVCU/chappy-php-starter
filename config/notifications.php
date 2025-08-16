12a75b4763e66283f140629f210ce2d98d10fdf3
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-15 20:01:50 -0400

    #36 - Registered log channel

--- config/notifications.php
+++ config/notifications.php
@@ -3,6 +3,7 @@
 return [
     'channels' => [
         'database' => Core\Lib\Notifications\Channels\DatabaseChannel::class,
+        'log' => Core\Lib\Notifications\Channels\LogChannel::class,
         'mail' => Core\Lib\Notifications\Channels\MailChannel::class
     ]
 ];
