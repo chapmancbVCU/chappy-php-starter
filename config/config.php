3c4afbce2b4b6632edf14b6b7185e9d6ab93d7c8
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-29 17:30:11 -0400

    Incremented version

--- config/config.php
+++ config/config.php
@@ -16,7 +16,7 @@ use Core\Lib\Utilities\Env;
     'default_layout' => Env::get('DEFAULT_LAYOUT') ?? 'main', // Default layout if not set in controller
 
     'app_domain' => Env::get('APP_DOMAIN') ?? '/', // Set this to '/' for a live server
-    'version' => '1.2.11',
+    'version' => '1.2.12',
     'site_title' => Env::get('SITE_TITLE') ?? 'My App', // Default site title if not set
     'menu_brand' => Env::get('MENU_BRAND') ?? 'My Brand', // Branding for menu
 
