84eb733aca245ac0a97fa94dcb227329a125e5ea
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-31 13:25:33 -0400

    Changed to dot notation

--- app/Controllers/ProfileController.php
+++ app/Controllers/ProfileController.php
@@ -47,7 +47,7 @@ class ProfileController extends Controller {
         $this->view->profileImages = $profileImages;
         $this->view->displayErrors = $user->getErrorMessages();
         $this->view->user = $user;
-        $this->view->render('profile/edit');
+        $this->view->render('profile.edit');
     }
 
     /**
