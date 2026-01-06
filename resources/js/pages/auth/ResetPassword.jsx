2fe8b025e0672b5d0f3531f66a54342d118569af
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-05 20:14:53 -0500

    Initial commit

---
+++ resources/js/pages/auth/ResetPassword.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Forms from "@chappy/components/Forms";
+import {PasswordComplexityRequirements} from '@chappy/components/PasswordComplexityRequirements';
+import route from "@chappy/utils/route";
+import documentTitle from "@chappy/utils/documentTitle";
+function ResetPassword() {
+
+    return (
+        <>
+        
+        </>
+    );
+}        
+export default ResetPassword;
