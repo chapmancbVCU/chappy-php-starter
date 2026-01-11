7b717b77c802a0be9cb9c174efcb48b064b5cc57
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-11 12:45:25 -0500

    Added two more test to file

--- resources/js/tests/unit/sum.test.js
+++ resources/js/tests/unit/sum.test.js
@@ -4,3 +4,13 @@ import { sum } from '../../../../sum.js'
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
 })
+
+
+test('adds 1 + 2 to equal 3 again', () => {
+  expect(sum(1, 2)).toBe(3)
+})
+
+
+test('adds 1 + 2 to equal 3 3rd time', () => {
+  expect(sum(1, 2)).toBe(3)
+})
