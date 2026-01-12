60ded200dd51a3ef8d5429b47cacc4b2cc6d2691
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-11 19:52:03 -0500

    Added tests to detect user's first name and another message on the page

--- resources/js/tests/view/home.test.jsx
+++ resources/js/tests/view/home.test.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { test, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import Index from '@/pages/home/Index';
 
-describe('HomeView', () => {
-  const user = { fname: "foo"}
+describe('Renders the welcome message with the correct user name', () => {
   it('renders heading', () => {
     const mockUser = {
       id: 1,
-      fname: "Jane Doe"
+      fname: 'Jane'
     }
     render(<Index user={mockUser}/>);
-    //expect(screen.getByText(/home/i)).toBeInTheDocument();
+    expect(screen.getByText(/Hello, Jane/i)).toBeInTheDocument();
+    expect(screen.getByText('This view is powered by React + Vite.')).toBeInTheDocument();
   });
 });
