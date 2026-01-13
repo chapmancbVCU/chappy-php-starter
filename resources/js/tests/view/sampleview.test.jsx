6e2d30a982b2323849128f99e29880e92b025644
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-12 19:06:04 -0500

    Rename test as part of formalization phase of creating samples for user

--- resources/js/tests/view/sampleview.test.jsx
+++ resources/js/tests/view/sampleview.test.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import Index from '@/pages/home/Index';
+import SampleView from '@chappy/testing/SampleView';
 
-describe('Renders the welcome message with the correct user name', () => {
+describe('Sample view test', () => {
   it('renders heading', () => {
     const mockUser = {
       id: 1,
       fname: 'Jane'
     }
-    render(<Index user={mockUser}/>);
+    render(<SampleView user={mockUser}/>);
     expect(screen.getByText(/Hello, Jane/i)).toBeInTheDocument();
-    expect(screen.getByText('This view is powered by React + Vite.')).toBeInTheDocument();
   });
 });
