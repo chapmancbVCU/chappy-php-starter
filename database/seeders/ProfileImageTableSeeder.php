bb0012491f61c7a2fb22704bdc7a88313b26631f
Chad Chapman <chad.chapman2010@gmail.com>
2026-02-16 18:53:10 -0500

    Updated to use new core factory class

--- database/seeders/ProfileImageTableSeeder.php
+++ database/seeders/ProfileImageTableSeeder.php
@@ -2,7 +2,7 @@
 namespace Database\Seeders;
 
 use Core\Lib\Database\Seeder;
-use Database\Factories\ProfileImageFactory;
+use Core\Lib\Database\Factories\ProfileImageFactory;
 
 /**
  * Class for generating profile images.
@@ -15,10 +15,10 @@ class ProfileImageTableSeeder extends Seeder {
      */
     public function run(): void {
         $factory = new ProfileImageFactory(1);
-        $factory->count(3);
+        $factory->count(1);
 
         $factory2 = new ProfileImageFactory(2);
-        $factory2->count(3);
+        $factory2->count(1);
         console_info("Finished seeding profileImage table.");
     }
 }
