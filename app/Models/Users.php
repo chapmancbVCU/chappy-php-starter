f24f54ccd15ee85d12743e98947798a6b5bcd204
Chad Chapman <chad.chapman2010@gmail.com>
2026-05-30 18:27:48 -0400

    #1 - Updated unique function call

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -139,7 +139,7 @@ class Users extends Model {
         $this->runValidation($this->required()->fieldName('fname')->max(150)->validate($this->fname));
         $this->runValidation($this->required()->fieldName('lname')->max(150)->validate($this->lname));
         $this->runValidation($this->required()->fieldName('email')->max(150)->email()->validate($this->email));
-        $this->runValidation($this->required()->fieldName('username')->min(6)->max(150)->unique(Users::class, 'username')->validate($this->username));
+        $this->runValidation($this->required()->fieldName('username')->min(6)->max(150)->unique(Users::class)->validate($this->username));
         $this->runValidation($this->required()->fieldName('password')->validate($this->password));
         
         if($this->isNew() || $this->changePassword) {
