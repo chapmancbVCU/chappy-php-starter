0958a4ff84863dfd90d6224d773a95bf5eecf4e6
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-17 06:27:40 -0400

    Update after performing testing

--- index.php
+++ index.php
@@ -4,17 +4,20 @@
  */
 
 use Core\Application;
+use Core\ErrorHandler;
+use Core\Lib\Logging\Logger;
+use Core\Router;
+use Core\SessionManager;
+
 // Define path related constants.
-// define('DS', DIRECTORY_SEPARATOR);
-// define('ROOT', dirname(__FILE__));
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
 
 
 
 // Load configuration and helper functions.
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/vendor/chappy-php/chappy-php-framework/src/scripts/bootstrap.php';
-Application::appStart();
-
 
 
 // Start PHP session
