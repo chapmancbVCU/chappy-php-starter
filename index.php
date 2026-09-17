bb272d4d8d9fd4aa220136fdf177e76a3249969f
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-16 21:51:18 -0400

    Moved init functions to Application class

--- index.php
+++ index.php
@@ -2,10 +2,8 @@
 /**
  * Application execution begins here.
  */
-use Core\Router;
-use Core\Lib\Logging\Logger;
-use Core\SessionManager;
-use Core\ErrorHandler;
+
+use Core\Application;
 
 // Define path related constants.
 define('DS', DIRECTORY_SEPARATOR);
@@ -14,21 +12,21 @@ define('ROOT', dirname(__FILE__));
 // Load configuration and helper functions.
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/vendor/chappy-php/chappy-php-framework/src/scripts/bootstrap.php';
-
+Application::appStart();
 
 // Start PHP session
-session_start();
+// session_start();
 
-// Set up error & exception handling
-ErrorHandler::initialize();
+// // Set up error & exception handling
+// ErrorHandler::initialize();
 
-// Start session & handle auto-login from Remember Me cookie
-SessionManager::initialize();
+// // Start session & handle auto-login from Remember Me cookie
+// SessionManager::initialize();
 
-// Perform routing.
-try {
-    Router::route();
-} catch (Exception $e) {
-    Logger::log("Unhandled Exception: " . $e->getMessage(), Logger::ERROR);
-    throw $e; // Let Whoops handle it
-}
+// // Perform routing.
+// try {
+//     Router::route();
+// } catch (Exception $e) {
+//     Logger::log("Unhandled Exception: " . $e->getMessage(), Logger::ERROR);
+//     throw $e; // Let Whoops handle it
+// }
