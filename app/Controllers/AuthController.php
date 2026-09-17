a0a0f2f1a2c4b007cfeda151947b0b5bc90f051b
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-16 21:51:35 -0400

    Updated controller for new login

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -2,10 +2,12 @@
 namespace App\Controllers;
 use Core\Controller;
 use App\Models\Users;
+use Core\Lib\Auth\LoginResult;
 use Core\Models\Login;
 use Core\Services\ACLService;
 use Core\Models\ProfileImages;
 use Core\Services\AuthService;
+use Core\Services\LoginService;
 use Core\Session;
 
 /**
@@ -27,7 +29,28 @@ class AuthController extends Controller {
             $loginModel->assign($this->request->get());
             $loginModel->validator();
             if($loginModel->validationPassed()){
-                $loginModel = AuthService::login($this->request, $loginModel, $_POST['username']);
+                $result = LoginService::attempt($this->request, $loginModel, $_POST['username']);
+
+                switch($result->status) {
+                    case LoginResult::SUCCESS:
+                        redirect(env('DEFAULT_CONTROLLER'));
+                        break;
+                    case LoginResult::NEEDS_RESET:
+                        redirect('auth.resetPassword', [$result->user->id]);
+                        break;
+                    case LoginResult::INACTIVE:
+                        flashMessage(Session::DANGER, 'Account is currently inactive');
+                        redirect('auth.login');
+                        break;
+                    case LoginResult::LOCKED:
+                        flashMessage(Session::DANGER, 'Your account has been locked due to too many failed login attempts.');
+                        redirect('auth.login');
+                        break;
+                    case LoginResult::INVALID:
+                        // Error message already added to $loginModel by the service.
+                        break;
+                }
+
             }
         }
 
