740cf16d3fe13c5b154a6a23ca381e81ad3cf3a5
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-16 21:58:23 -0400

    Moved creation of constants to core

--- index.php
+++ index.php
@@ -4,22 +4,16 @@
  */
 
 use Core\Application;
-
 // Define path related constants.
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(__FILE__));
+// define('DS', DIRECTORY_SEPARATOR);
+// define('ROOT', dirname(__FILE__));
+
+
 
 // Load configuration and helper functions.
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/vendor/chappy-php/chappy-php-framework/src/scripts/bootstrap.php';
 Application::appStart();
-
-
-
-
-
-
-
 
 
 
