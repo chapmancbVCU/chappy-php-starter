359816e6c91cdd416dca2d45db31db77c9e56210
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-18 06:30:23 -0400

    Removed commented code after testing

--- index.php
+++ index.php
@@ -4,36 +4,12 @@
  */
 
 use Core\Application;
-use Core\ErrorHandler;
-use Core\Lib\Logging\Logger;
-use Core\Router;
-use Core\SessionManager;
 
 // Define path related constants.
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__));
 
-
-
 // Load configuration and helper functions.
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/vendor/chappy-php/chappy-php-framework/src/scripts/bootstrap.php';
 Application::appStart();
-
-
-// Start PHP session
-// session_start();
-
-// // Set up error & exception handling
-// ErrorHandler::initialize();
-
-// // Start session & handle auto-login from Remember Me cookie
-// SessionManager::initialize();
-
-// // Perform routing.
-// try {
-//     Router::route();
-// } catch (Exception $e) {
-//     Logger::log("Unhandled Exception: " . $e->getMessage(), Logger::ERROR);
-//     throw $e; // Let Whoops handle it
-// }
