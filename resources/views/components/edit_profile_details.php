345e0399dfccc78b40d89f5f060d4721014221fd
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-19 15:41:20 -0400

    Switched to new text global

--- resources/views/components/edit_profile_details.php
+++ resources/views/components/edit_profile_details.php
@@ -1,6 +1,7 @@
-<?= input('text', "First Name", 'fname', $this->user->fname, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
-<?= input('text', "Last Name", 'lname', $this->user->lname, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
+<?= text("First Name", 'fname', $this->user->fname, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
+<?= text("Last Name", 'lname', $this->user->lname, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
 <?= email("Email", 'email', $this->user->email, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
+
 
 <?= textarea("Description", 
     'description', 
