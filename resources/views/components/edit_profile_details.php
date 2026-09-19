5fa81ecd96aa20bec8169e47a193809472293d2e
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-19 18:16:15 -0400

    Switched to new global functions

--- resources/views/components/edit_profile_details.php
+++ resources/views/components/edit_profile_details.php
@@ -2,10 +2,11 @@
 <?= text("Last Name", 'lname', $this->user->lname, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
 <?= email("Email", 'email', $this->user->email, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
 
-
 <?= textarea("Description", 
     'description', 
     $this->user->description, 
     ['class' => 'form-control input-sm', 'placeholder' => 'Describe yourself here...'], 
     ['class' => 'form-group mb-3']); 
 ?>
+
+<?= week("Email", 'email', '', ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
