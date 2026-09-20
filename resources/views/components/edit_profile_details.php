b1b79ce3132e97bec3439951b70d29dbd3adc716
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-20 15:34:30 -0400

    Updated to use new fileSelector

--- resources/views/components/edit_profile_details.php
+++ resources/views/components/edit_profile_details.php
@@ -1,3 +1,7 @@
+
+<?= currency("test", 'test', '$', $this->currency, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
+
+
 <?= text("First Name", 'fname', $this->user->fname, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
 <?= text("Last Name", 'lname', $this->user->lname, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
 <?= email("Email", 'email', $this->user->email, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
@@ -8,5 +12,3 @@
     ['class' => 'form-control input-sm', 'placeholder' => 'Describe yourself here...'], 
     ['class' => 'form-group mb-3']); 
 ?>
-
-<?= week("Email", 'email', '', ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
