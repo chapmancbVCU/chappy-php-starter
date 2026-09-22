d77bca3447d73c570f128ebd12bafb02fc2b2ebf
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-21 21:45:35 -0400

    Testing

--- app/Controllers/ProfileController.php
+++ app/Controllers/ProfileController.php
@@ -48,18 +48,21 @@ class ProfileController extends Controller {
             }
         }
 
+        $users = Users::find();
+        // dump($users);
+        $this->view->users = $users;
         // Toggle comments to use React.js
-        // $this->view->profileImages = $profileImages;
-        // $this->view->displayErrors = $user->getErrorMessages();
-        // $this->view->user = $user;
-        // $this->view->render('profile.edit');
+        $this->view->profileImages = $profileImages;
+        $this->view->displayErrors = $user->getErrorMessages();
+        $this->view->user = $user;
+        $this->view->render('profile.edit');
 
-        $props = [
-            'user' => $user,
-            'errors' => $user->getErrorMessages(),
-            'profileImages' => $profileImages,
-        ];
-        $this->view->renderJsx('profile.Edit', $props);
+        // $props = [
+        //     'user' => $user,
+        //     'errors' => $user->getErrorMessages(),
+        //     'profileImages' => $profileImages,
+        // ];
+        // $this->view->renderJsx('profile.Edit', $props);
     }
 
     /**
--- resources/views/components/edit_profile_details.php
+++ resources/views/components/edit_profile_details.php
@@ -1,5 +1,12 @@
 
-<?= currency("test", 'test', '$', $this->currency, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
+<!-- <?= radio("Email", 'contact', 'email', false, ['class' => 'form-group mb-3']) ?>
+<?= radio("Phone", 'contact', 'phone', false, ['class' => 'form-group mb-3']) ?> -->
+
+<!-- <?= radioGroup('users', ['User1', 'User2'], $this->users, 'username', [''], ['class' => 'form-group mb-3']) ?> -->
+
+
+<?= dataListDate('Choose a browser from the list', 'browser', 'browsers', '', ['2026-09-21', '2026-09-22', '2026-09-23'], ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
+
 
 
 <?= text("First Name", 'fname', $this->user->fname, ['class' => 'form-control input-sm'], ['class' => 'form-group mb-3']) ?>
